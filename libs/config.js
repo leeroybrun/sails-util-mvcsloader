efde5453867358955d7596fb567aa23b8e92b5c7
jaumard <ioquoo3H>
2015-07-02 10:49:54 +0200

    Fix merge configs, change build-dictionary.aggragate to include-all solution

--- libs/config.js
+++ libs/config.js
@@ -2,20 +2,18 @@
  * Load config from a directory into a Sails app
  */
 
-var buildDictionary = require('sails-build-dictionary');
 var _ = require('lodash');
 module.exports = function (sails, dir) {
-    buildDictionary.aggregate({
+    var configs = require('include-all')({
         dirname: dir,
         exclude: ['locales', 'local.js', 'local.json', 'local.coffee', 'local.litcoffee'],
-        excludeDirs: /(locales|env)$/,
         filter: /(.+)\.(js|json|coffee|litcoffee)$/,
-        identity: false
-    }, function (err, configs) {
-        sails.config = _.merge(configs, sails.config, function (a, b) {
-            if (_.isArray(a)) {
-                return a.concat(b);
-            }
-        });
+        excludeDirs: /(locales|env)$/,
+    });
+
+    sails.config = _.merge(configs, sails.config, function (a, b) {
+        if (_.isArray(a)) {
+            return a.concat(b);
+        }
     });
 };
