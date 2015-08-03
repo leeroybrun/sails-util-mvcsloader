748fdaa42ac1397c7a35bc75f4dd584e7e4555a6
jaumard <ioquoo3H>
2015-08-03 16:44:47 +0200

    Revert et fix config loader to have same config files as sails

--- libs/config.js
+++ libs/config.js
@@ -2,18 +2,20 @@
  * Load config from a directory into a Sails app
  */
 
+var buildDictionary = require('sails-build-dictionary');
 var _ = require('lodash');
 module.exports = function (sails, dir) {
-    var configs = require('include-all')({
+    buildDictionary.aggregate({
         dirname: dir,
         exclude: ['locales', 'local.js', 'local.json', 'local.coffee', 'local.litcoffee'],
-        filter: /(.+)\.(js|json|coffee|litcoffee)$/,
         excludeDirs: /(locales|env)$/,
-    });
-
-    sails.config = _.merge(configs, sails.config, function (a, b) {
-        if (_.isArray(a)) {
-            return a.concat(b);
-        }
+        filter: /(.+)\.(js|json|coffee|litcoffee)$/,
+        identity: false
+    }, function (err, configs) {
+        sails.config = _.merge(configs, sails.config, function (a, b) {
+            if (_.isArray(a)) {
+                return a.concat(b);
+            }
+        });
     });
 };
